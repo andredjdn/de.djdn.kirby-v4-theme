9b885d86a54fdef55edda42b28132e675481e19a
André Dujardin <hello@djdn.de>
2024-12-19 18:05:05 +0100

    Fixed responsive table behaviour

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -6,6 +6,9 @@ $alt     = $block->alt()->or($image->alt());
 $caption = $block->caption()->or($image->caption());
 $crop    = $block->crop()->isTrue();
 $link    = $block->link()->or($image->link());
+$photographer = $block->photographer()->or($image->photographer());
+$tool    = $block->tool()->or($image->tool());
+$license = $block->license()->or($image->license());
 $ratio   = $block->ratio()->or('auto');
 $src     = null;
 
@@ -17,18 +20,42 @@ if ($block->location() == 'web') {
 
 ?>
 <?php if ($src) : ?>
-    <figure<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
-        <?php if ($link->isNotEmpty()) : ?>
-            <a href="<?= Str::esc($link->toUrl()) ?>">
-                <img src="<?= $src ?>" alt="<?= $alt->esc() ?>">
-            </a>
-        <?php else : ?>
-            <img src="<?= $src ?>" alt="<?= $alt->esc() ?>">
-        <?php endif ?>
-        <?php if ($caption->isNotEmpty()) : ?>
-            <figcaption>
-                <?= $caption ?>
-            </figcaption>
+    <figure class="image"<?= Html::attr(['data-ratio' => $ratio, 'data-crop' => $crop], null, ' ') ?>>
+        <img src="<?= $src ?>" alt="<?= $alt->esc() ?>">
+        <?php if ($photographer->isNotEmpty() || $tool->isNotEmpty() || $license->isNotEmpty() || $caption->isNotEmpty()) : ?>
+        <figcaption>
+            <div class="flex">
+                <?php if ($photographer->isNotEmpty()) : ?>
+                <div>
+                    <label>Urheber*in:</label>
+                    <?php if ($link->isNotEmpty()) : ?>
+                        <a href="<?= $link ?>"><?= $photographer ?></a>
+                    <?php else : ?>
+                        <?= $photographer ?>
+                    <?php endif ?>
+                </div>
+                <?php endif ?>
+                <?php if ($tool->isNotEmpty()) : ?>
+                    <div>
+                        <label>Tool:</label>
+                        <?php if ($link->isNotEmpty()) : ?>
+                            <a href="<?= Str::esc($link->toUrl()) ?>"><?= $tool ?></a>
+                        <?php else : ?>
+                            <?= $tool ?>
+                        <?php endif ?>
+                    </div>
+                <?php endif ?>
+                <?php if ($license->isNotEmpty()) : ?>
+                <div>
+                    <label>Lizenz:</label>
+                    <?= $license ?>
+                </div>
+                <?php endif ?>
+            </div>
+            <?php if ($caption->isNotEmpty()) : ?>
+            <div class="description"><?= $caption ?></div>
+            <?php endif ?>
+        </figcaption>
         <?php endif ?>
     </figure>
 <?php endif ?>
--- site/snippets/blocks/table-contact.php
+++ site/snippets/blocks/table-contact.php
@@ -2,22 +2,24 @@
 $rows = $block->rows()->toStructure();
 if($rows->isNotEmpty()):
 ?>
-<table>
-    <tr>
-        <th>#</th>
-        <th>Key</th>
-        <th>Fingerprint</th>
-    </tr>
-    <?php foreach ($rows as $row) : ?>
-    <tr>
-        <td><?= $row->value()->kirbytextinline() ?></td>
-        <?php if ($row->key()->isNotEmpty()) : ?>
-        <td><?= $row->key()->kirbytextinline() ?></td>
-        <?php endif ?>
-        <?php if ($row->fingerprint()->isNotEmpty()) : ?>
-        <td><code><?= $row->fingerprint()->html() ?></code></td>
-        <?php endif ?>
-    </tr>
-    <?php endforeach ?>
+<div>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Key</th>
+            <th>Fingerprint</th>
+        </tr>
+        <?php foreach ($rows as $row) : ?>
+        <tr>
+            <td><?= $row->value()->kirbytextinline() ?></td>
+            <?php if ($row->key()->isNotEmpty()) : ?>
+            <td><?= $row->key()->kirbytextinline() ?></td>
+            <?php endif ?>
+            <?php if ($row->fingerprint()->isNotEmpty()) : ?>
+            <td><code><?= $row->fingerprint()->html() ?></code></td>
+            <?php endif ?>
+        </tr>
+        <?php endforeach ?>
     </table>
+</div>
 <?php endif; ?>
